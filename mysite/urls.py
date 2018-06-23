1ed43ce48812aa7884cb2b2c88f3a1ddd87707a4
Andrea <andrea.horavova@gmail.com>
2018-06-23 14:07:39 +0200

    Add html template

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,8 @@
+from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
-# Examples:
-# url(r'^$', 'mysite.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 
 urlpatterns = [
-
     url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
